faffdeb7bd9da6e423392ecf67066ed9324642d0
John D Wells <johndwells@gmail.com>
2011-12-11 19:05:08 +0000

    Version 1.1.1: Add query caching; misc formatting/commenting.

--- README.md
+++ README.md
@@ -1,57 +1,69 @@
-#Many Assets
+# Many Assets
 
 * Author: [John D Wells](http://johndwells.com)
-
-## Version 1.1.0 (consider beta until I say otherwise)
+* Version 1.1.1 (consider beta until I say otherwise)
 
 Requires:
 
-* [ExpressionEngine 2](http://expressionengine.com/)
+* PHP5
+* [ExpressionEngine](http://expressionengine.com/) 2.1.3 or later
 * [P&Ts Assets](http://pixelandtonic.com/assets)
+
 
 ## Description
 
-A simple plugin that allows you to retrieve [Pixel&Tonic's Assets](http://pixelandtonic.com/assets) attached to more than one entry. Simply pass a pipe- or comma-delimited list of entry ids, add parse="inward", and be on your way. 
+A simple plugin that allows you to retrieve [Pixel&Tonic's Assets](http://pixelandtonic.com/assets) from across many Entries, and/or across many custom fields.
+
+Simply pass a pipe- or comma-delimited list of entry ids, add parse="inward", and be on your way. 
+
+Aren't you glad I didn't call it 'ManEE Assets'?
+
 
 ## Installation
 
-1. Copy the many_assets folder to ./system/expressionengine/third_party/
+1. Be sure Assets is installed and configured to your liking
+2. Copy the many_assets folder to ./system/expressionengine/third_party/
+
 
 ## Required Parameters
 
-### entry_ids="..."
+**`entry_ids="..."`**
 
 A pipe- or string-delimited list of entry IDs.
 
-### parse="inward"
+**`parse="inward"`**
 
 This is required for the plugin to parse in the correct order.
+
 
 ## Optional Parameters
 
 Accepts all of Asset's documented parameters, plus:
 
-### field_names="…"
+**`field_names="..."`**
 
 A pipe- or string-delimited list of field names that the lookup should be limited to.
 
 *beta*: To limit to specific matrix columns, format should be "field_name:column_name"
 
 
-## Example - Basic
+## Usage Examples #
+
+### Basic #
 
 	{exp:many_assets entry_ids="420,1976,2011" parse="inward"}
 		...
 	{/exp:many_assets}
 
 
-## Example - Limit to field name & matrix col
+### Limit to field name & matrix col #
 
 	{exp:many_assets entry_ids="420,1976,2011" field_names="cf_page_slideshow,cf_page_matrix:file" parse="inward"}
 		...
 	{/exp:many_assets}
 
-## Example - Combined with Playa and CE Img
+
+### Combined with Playa and CE Img #
 
 	{exp:many_assets entry_ids="{exp:playa:parent_ids entry_id='420' delimiter=','}" parse="inward" prefix="asset" limit="5" orderby="random"}
 
@@ -65,9 +77,3 @@ A pipe- or string-delimited list of field names that the lookup should be limite
 	{/exp:many_assets}
 
 
-## TODO
-
-* Cache queries to be considerate?
-* Verify Matrix is installed before making query
-* Debugging mode
-* Improve robustness against potential errors
--- system/expressionengine/third_party/many_assets/pi.many_assets.php
+++ system/expressionengine/third_party/many_assets/pi.many_assets.php
@@ -1,20 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * ExpressionEngine - by EllisLab
- *
- * @package		ExpressionEngine
- * @author		ExpressionEngine Dev Team
- * @copyright	Copyright (c) 2003 - 2011, EllisLab, Inc.
- * @license		http://expressionengine.com/user_guide/license.html
- * @link		http://expressionengine.com
- * @since		Version 2.0
- * @filesource
- */
- 
-// ------------------------------------------------------------------------
-
-/**
  * Many Assets Plugin
  *
  * @package		ExpressionEngine
@@ -26,13 +12,12 @@
 
 $plugin_info = array(
 	'pi_name'		=> 'Many Assets',
-	'pi_version'	=> '1.1.0',
+	'pi_version'	=> '1.1.1',
 	'pi_author'		=> 'John D Wells',
 	'pi_author_url'	=> 'http://johndwells.com',
-	'pi_description'=> 'Retrieve Assets from across multiple Entries',
+	'pi_description'=> 'Retrieve P&T Assets from across many Entries, and/or across many custom fields.',
 	'pi_usage'		=> Many_assets::usage()
 );
-
 
 class Many_assets {
 
@@ -43,76 +28,66 @@ class Many_assets {
 	 */
 	public function __construct()
 	{
+
+		// Obviously
 		$this->EE =& get_instance();
+
+
+
+		/*
+		 * create and/or reference our cache
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		if ( ! isset($this->EE->session->cache['many_assets']))
+        {
+            $this->EE->session->cache['many_assets'] = array();
+        }
+        $this->cache =& $this->EE->session->cache['many_assets'];
     
-		// if successful, this will be filled with assets		
-		$files = array();
+
 
     	/*
-    	 * fetch params
+    	 * fetch required param(s) - return immediately if any are not provided
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
     	 */
-    	$entry_ids		= $this->EE->TMPL->fetch_param('entry_ids');
+		if (($entry_ids	= $this->EE->TMPL->fetch_param('entry_ids', FALSE)) == FALSE) return;
+
+
+
+    	/*
+    	 * fetch optional params
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+    	 */
     	$field_names	= $this->EE->TMPL->fetch_param('field_names', FALSE);
     	$orderby		= $this->EE->TMPL->fetch_param('orderby', FALSE);
     	$sort			= $this->EE->TMPL->fetch_param('sort', '');
-    	$limit			= $this->EE->TMPL->fetch_param('limit', 10);
+    	$limit			= $this->EE->TMPL->fetch_param('limit', 0);
     	$offset			= $this->EE->TMPL->fetch_param('offset', 0);
 
-    	/*
-    	 * standardise delimiter format
-    	 */
+
+
+		/*
+		 * Format/standardise params
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		
 		$entry_ids = trim($entry_ids, ',|');
     	if(strpos($entry_ids, '|') !== FALSE)
     	{
     		$entry_ids = str_replace('|', ',', $entry_ids);
     	}
 
-		/*
-		 * Limit query to field(s)?
-		 */
-		$field_ids = array();
-		$col_ids = array();
-		if($field_names)
-		{
-			// standardise delimiter format
-			$field_names = trim($field_names, ',|');
-	    	if(strpos($field_names, '|') !== FALSE)
-	    	{
-	    		$field_names = str_replace('|', ',', $field_names);
-	    	}
+		$field_names = trim($field_names, ',|');
+    	if(strpos($field_names, '|') !== FALSE)
+    	{
+    		$field_names = str_replace('|', ',', $field_names);
+    	}
 
-    		// loop through each, because if a matrix col then we need extra steps
-    		foreach(explode(',', $field_names) as $name)
-    		{
-    			if(strpos($field_names, ':') !== FALSE)
-    			{
-    				/*
-    				 * TODO: Verify that Matrix is installed & available
-    				 */
-    				 list($field_name, $col_name) = explode(':', $field_names);
-    				 $sql = 'SELECT mc.col_id, mc.field_id
-    				 			FROM exp_matrix_cols mc JOIN exp_channel_fields cf ON mc.field_id = cf.field_id
-    				 			WHERE cf.field_name = "' . $field_name . '"
-    				 			AND mc.col_name = "' . $col_name . '"';
-			    		$query = $this->EE->db->query($sql);
-						if($query->num_rows())
-						{
-							$field_ids[] = $query->row()->field_id;
-							$col_ids[] = $query->row()->col_id;
-						}
-    			}
-    			else
-    			{
-		    		$sql = 'SELECT field_id FROM exp_channel_fields WHERE field_name = "' . $field_names . '"';
-		    		$query = $this->EE->db->query($sql);
-					if($query->num_rows())
-					{
-						$field_ids[] = $query->row()->field_id;
-					}
-    			}
-    		}
-		}
-    	
+		$orderby = strtolower($orderby);
     	if($orderby == 'random')
     	{
     		$orderby = 'RAND()';
@@ -121,78 +96,158 @@ class Many_assets {
     		$sort = '';
     	}
     	
-    	// make sure we have a value to use
-		if($entry_ids)
+    	$limit = intval($limit);
+    	$offset = intval($offset);
+
+
+
+		/*
+		 * Limit query to field(s)?
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		$field_ids = array();
+		$col_ids = array();
+		if($field_names)
 		{
-			/*
-			 * build our query
-			 */
-			$sql = 'SELECT DISTINCT a.asset_id, a.*
-				FROM exp_assets a
-				JOIN exp_assets_entries ae ON a.asset_id = ae.asset_id
-				WHERE ae.entry_id IN(' . $entry_ids . ')';
-			
-			if($field_ids)
-			{
-				$field_ids = implode(',', $field_ids);
-				$sql .= ' AND ae.field_id IN(' . $field_ids . ')';
-			}
-			
-			if($col_ids)
-			{
-				$col_ids = implode(',', $col_ids);
-				$sql .= ' AND ae.col_id IN(' . $col_ids . ')';
-			}
-			
-			if($orderby)
-			{
-				$sql .= ' ORDER BY ' . $orderby . ' ' . $sort;
-			}
-			
-			if(intval($limit) > 0)
-			{
-				$sql .= ' LIMIT ' . intval($limit);
-			}
-			
-			if(intval($offset) > 0)
-			{
-				$sql .= ' OFFSET ' . intval($limit);
-			}
-			
-			// TODO: cache query?
-			
-			// loop through our returned result
-			if($rows = $this->EE->db->query($sql)->result_array())
-			{
-			
-				// Include dependency classes
-				if ( ! class_exists('EE_Fieldtype'))
-				{
-					include_once (APPPATH . 'fieldtypes/EE_Fieldtype' . EXT);
-				}
 
-				if ( ! class_exists('Assets_ft'))
-				{
-					include_once PATH_THIRD . 'assets/ft.assets.php';
-				}
-
-				// heavy lifting
-				$Assets_ft = new Assets_ft();
-				foreach ($rows as $row)
-				{
-					$file = $Assets_ft->helper->get_file($row['file_path']);
-		
-					if ($file->exists())
+    		// loop through each, because if a matrix col then we need extra steps
+    		foreach(explode(',', $field_names) as $name)
+    		{
+    			if(strpos($field_names, ':') !== FALSE)
+    			{
+    				// Only run query if our matrix table exists
+    				if($this->EE->db->table_exists('exp_matrix_cols'))
+    				{
+	    				list($field_name, $col_name) = explode(':', $field_names);
+	    				$sql = 'SELECT mc.col_id, mc.field_id
+	    							FROM exp_matrix_cols mc JOIN exp_channel_fields cf ON mc.field_id = cf.field_id
+	    							WHERE cf.field_name = "' . $field_name . '"
+	    							AND mc.col_name = "' . $col_name . '" LIMIT 1';
+				    		$query = $this->EE->db->query($sql);
+							if($query->num_rows())
+							{
+								$field_ids[] = $query->row()->field_id;
+								$col_ids[] = $query->row()->col_id;
+							}
+							
+							// waste not, want not
+							$query->free_result();
+    				}
+    			}
+    			else
+    			{
+		    		$sql = 'SELECT field_id FROM exp_channel_fields WHERE field_name = "' . $field_names . '" LIMIT 1';
+		    		$query = $this->EE->db->query($sql);
+					if($query->num_rows())
 					{
-						$file->set_row($row);
-						
-						$files[] = $file;
+						$field_ids[] = $query->row()->field_id;
 					}
+
+					// waste not, want not
+					$query->free_result();
+    			}
+    		}
+		}
+
+
+
+		/*
+		 * build our query
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		$sql = 'SELECT DISTINCT a.asset_id, a.*
+			FROM exp_assets a
+			JOIN exp_assets_entries ae ON a.asset_id = ae.asset_id
+			WHERE ae.entry_id IN(' . $entry_ids . ')';
+
+		if($field_ids)
+		{
+			$field_ids = implode(',', $field_ids);
+			$sql .= ' AND ae.field_id IN(' . $field_ids . ')';
+		}
+		
+		if($col_ids)
+		{
+			$col_ids = implode(',', $col_ids);
+			$sql .= ' AND ae.col_id IN(' . $col_ids . ')';
+		}
+		
+		if($orderby)
+		{
+			$sql .= ' ORDER BY ' . $orderby . ' ' . $sort;
+		}
+		
+		if($limit > 0)
+		{
+			$sql .= ' LIMIT ' . $limit;
+		}
+		
+		if($offset > 0)
+		{
+			$sql .= ' OFFSET ' . $limit;
+		}
+
+
+
+		/*
+		 * Fetch & cache our query if not already cached
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		$key = serialize($sql);
+		if ( ! isset($this->_cache[$key]))
+		{
+			$query = $this->EE->db->query($sql);
+			$this->_cache[$key] = $query->result_array();
+			$query->free_result();
+		}
+
+
+
+		/*
+		 * process whatever we've found
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
+		$files = array();
+		if($this->_cache[$key])
+		{
+		
+			// Include dependency classes
+			if ( ! class_exists('EE_Fieldtype'))
+			{
+				include_once (APPPATH . 'fieldtypes/EE_Fieldtype' . EXT);
+			}
+
+			if ( ! class_exists('Assets_ft'))
+			{
+				include_once PATH_THIRD . 'assets/ft.assets.php';
+			}
+
+			// heavy lifting
+			$Assets_ft = new Assets_ft();
+			foreach ($this->_cache[$key] as $row)
+			{
+				$file = $Assets_ft->helper->get_file($row['file_path']);
+	
+				if ($file->exists())
+				{
+					$file->set_row($row);
+					
+					$files[] = $file;
 				}
 			}
 		}
-	
-		// so, what did we find?
+
+
+
+		/*
+		 * What to return?
+		 * ----------------------------------------------------
+		 * ----------------------------------------------------
+		 */
 		if(count($files) > 0)
 		{
 			$this->return_data = $Assets_ft->replace_tag($files, $this->EE->TMPL->tagparams, $this->EE->TMPL->tagdata);
@@ -204,7 +259,10 @@ class Many_assets {
 		// release the hounds!
 		return $this->return_data;
     }
-	
+
+
+
+
 	/**
 	 * Plugin Usage
 	 */
